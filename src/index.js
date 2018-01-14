d4d058537f2f481ace87b36a8a1923f8f0da0fb2
iamqaz <jonathon.d.meyers@gmail.com>
2018-01-14 11:54:59 +1100

    NEW: Add material-ui for theme

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter, Switch, Route } from 'react-router-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import './index.css';
 import App from './components/App/';
 import RecipeList from './components/RecipeList';
@@ -9,15 +10,17 @@ import NotFound from './components/NotFound';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-  <HashRouter>
-    <App>
-      <Switch>
-        <Route exact path="/" component={RecipeList} />
-        <Route path="/recipes/:id" component={Recipe} />
-        <Route component={NotFound} />
-      </Switch>
-    </App>
-  </HashRouter>, 
+  <MuiThemeProvider>
+    <HashRouter>
+      <App>
+        <Switch>
+          <Route exact path="/" component={RecipeList} />
+          <Route path="/recipes/:id" component={Recipe} />
+          <Route component={NotFound} />
+        </Switch>
+      </App>
+    </HashRouter>
+  </MuiThemeProvider>, 
   document.getElementById('root')
 );
 registerServiceWorker();
